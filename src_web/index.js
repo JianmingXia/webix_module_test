77d4ee909b513cf99c2cb1bdb80d3d6ae2a0e758
Ryoma <xiajianming@quqi.com>
2017-03-13 20:01:46 +0800

    add module init

---
+++ src_web/index.js
@@ -0,0 +1,76 @@
+var webix = require("../lib/webix/webix-button");
+require("../lib/webix/webix-window");
+require("../lib/webix/webix-spacer");
+require("../lib/webix/webix-layout");
+
+var button = webix.ui({
+    cols:[
+        {
+            view:"button",
+            value:"Button",
+            inputWidth:100
+        },
+        {
+            view:"button",
+            id:"my_button",
+            value:"Button",
+            inputWidth:100
+        },
+        {
+            view:"button",
+            value:"Button",
+            inputWidth:100
+        }
+    ]
+});
+
+// 浮于右边
+// webix.ui({
+//     view:"window",
+//     head:"My Window",
+//     autofit: false,
+//     window_point: true,
+//     window_point_pos: "left",
+//     body:{
+//         template:"Some text"
+//     }
+// }).show($$("my_button").getNode(),
+//     {
+//       pos: "right",
+//       x: 100
+//     }
+// );
+
+// 浮于左边
+// webix.ui({
+//     view:"window",
+//     head:"My Window",
+//     autofit: false,
+//     window_point: true,
+//     window_point_pos: "right",
+//     body:{
+//         template:"Some text"
+//     }
+// }).show($$("my_button").getNode(),
+//     {
+//           pos: "left",
+//           x: 12
+//     }
+// );
+
+// 浮于下方
+webix.ui({
+    view:"window",
+    head:"My Window",
+    autofit: false,
+    window_point: true,
+    window_point_pos: "top",
+    body:{
+        template:"Some text"
+    }
+}).show($$("my_button").getNode(),
+    {
+          pos: "bottom",
+          x: 12
+    }
+);
---
+++ webpack.config.js
@@ -0,0 +1,76 @@
+var webpack = require('webpack');
+var ExtractTextPlugin = require('extract-text-webpack-plugin');
+var HtmlWebpackPlugin = require('html-webpack-plugin');
+var path = require('path');
+var version = require('yargs').argv.version || 'trunk';
+// var publicPath = 'http://cdn.t.quqi.com/' + version + '/';
+var publicPath = 'http://127.0.0.1:20004/' + version + '/';
+
+function make() {
+  var entry = {
+    app: "./src_web/index.js",
+    vendor: [
+      "lodash"
+    ]
+  };
+
+  var plugins = [
+    new webpack.ProvidePlugin({
+      _: "lodash"
+    }),
+    new ExtractTextPlugin('css/[contenthash].css', {
+      allChunks: true
+    }),
+    new HtmlWebpackPlugin({
+      template: './src_web/index.html',
+      favicon: './src_web/favicon.ico',
+      faviconPath: publicPath + 'favicon.ico',
+    }),
+    new webpack.optimize.CommonsChunkPlugin("vendor", "vendor.bundle.js")
+  ];
+  return {
+
+    entry: entry,
+
+    output: {
+      filename: 'module.[hash].js',
+      path: path.resolve('./dist_web/' + version + '/'),
+      publicPath: publicPath,
+      libraryTarget: 'umd'
+    },
+
+    module: {
+      loaders: [{
+        test: /\.js$/,
+        loader: 'babel-loader?optional[]=runtime&stage=0',
+        exclude: /(node_modules|bower_components|spread|file_saver)/
+      }, {
+        test: /(((.*)webix(.*))|((.*)Gcspread(.*))|((.*)ueditor(.*))|((.*)pdfjs(.*)))\.scss$/,
+        loader: ExtractTextPlugin.extract('style-loader', 'css-loader!autoprefixer-loader?{browsers:["last 2 version", "ie >= 10"]}!sass-loader', {
+          publicPath: '../'
+        })
+      }, {
+        test: /(.*)web_css(.*)\.scss$/,
+        loader: ExtractTextPlugin.extract('style-loader', 'css-loader?modules&importLoaders=2&localIdentName=[name]__[local]___[hash:base64:5]!autoprefixer-loader?{browsers:["last 2 version", "ie >= 10"]}!sass-loader', {
+          publicPath: '../'
+        })
+      }, {
+        test: /\.html$/,
+        loader: 'html'
+      }, {
+        test: /\.jade$/,
+        loader: "jade-loader?self"
+      }, {
+        test: /\.(woff|woff2|eot|ttf|svg|png|gif)$/,
+        loader: 'url-loader?limit=10000&name=[path][name]_[hash:base64:5].[ext]'
+      }]
+    },
+    resolve: {
+      alias: {
+        lodash: "lodash/lodash.min.js"
+      }
+    },
+    plugins: plugins
+  };
+}
+module.exports = make();
