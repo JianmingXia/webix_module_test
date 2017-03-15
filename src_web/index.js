136c6197c3b2efb565365e4347f6f7b484e7c0bd
Ryoma <xiajianming@quqi.com>
2017-03-15 09:47:14 +0800

    list edit
    add property noscroll

--- src_web/index.js
+++ src_web/index.js
@@ -2,32 +2,64 @@ var webix = require("../lib/webix/webix-button");
 require("../lib/webix/webix-window");
 require("../lib/webix/webix-spacer");
 require("../lib/webix/webix-layout");
+require("../lib/webix/webix-list");
 
 
-var button = webix.ui({
-    rows: [
-        {
-            height: 300
-        },
-        {
-            cols:[
-                {
-                    view:"button",
-                    value:"Button",
-                    inputWidth:100
-                },
-                {
-                    view:"button",
-                    id:"my_button",
-                    value:"Button",
-                    inputWidth:100
-                },
-                {
-                    view:"button",
-                    value:"Button",
-                    inputWidth:100
-                }
-            ]
-        }
-    ]
+// var button = webix.ui({
+//     rows: [
+//         {
+//             height: 300
+//         },
+//         {
+//             cols:[
+//                 {
+//                     view:"button",
+//                     value:"Button",
+//                     inputWidth:100
+//                 },
+//                 {
+//                     view:"button",
+//                     id:"my_button",
+//                     value:"Button",
+//                     inputWidth:100
+//                 },
+//                 {
+//                     view:"button",
+//                     value:"Button",
+//                     inputWidth:100
+//                 }
+//             ]
+//         }
+//     ]
+// });
+if (!webix.env.touch && webix.ui.scrollSize) {
+    webix.CustomScroll.init();
+}
+
+var list = webix.ui({
+  view:"list",
+  width:250,
+  height:200,
+  template:"#title#",
+  id:"list",
+  select:true,
+  // scroll: false,
+  data:[
+    { id:1, title:"Item 1"},
+    { id:2, title:"Item 2"},
+    { id:3, title:"Item 3"},
+        { id:11, title:"Item 1"},
+    { id:12, title:"Item 2"},
+    { id:13, title:"Item 3"},
+    { id:112, title:"Item 2"},
+    { id:113, title:"Item 3"}
+  ]
 });
+
+
+// document.body.onmousewheel = function(event) {
+//     event = event || window.event;
+//     console.dir(event);
+//     event.stopPropagation();
+//     event.preventDefault();
+// };
