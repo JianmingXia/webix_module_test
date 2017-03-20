a8a9544e0eba3d0601f59a94c6c11369a020509c
Ryoma <xiajianming@quqi.com>
2017-03-20 15:10:26 +0800

    add autoheight

--- src_web/index.js
+++ src_web/index.js
@@ -1,8 +1,9 @@
-var webix = require("../lib/webix/webix-button");
-require("../lib/webix/webix-window");
-require("../lib/webix/webix-spacer");
-require("../lib/webix/webix-layout");
-require("../lib/webix/webix-list");
+// var webix = require("../lib/webix/webix-button");
+// require("../lib/webix/webix-window");
+// require("../lib/webix/webix-spacer");
+// require("../lib/webix/webix-layout");
+// require("../lib/webix/webix-list");
+require("../lib/webix/webix-label");
 
 
 // var button = webix.ui({
@@ -32,29 +33,29 @@ require("../lib/webix/webix-list");
 //         }
 //     ]
 // });
-if (!webix.env.touch && webix.ui.scrollSize) {
-    webix.CustomScroll.init();
-}
-
-var list = webix.ui({
-  view:"list",
-  width:250,
-  height:200,
-  template:"#title#",
-  id:"list",
-  select:true,
-  // scroll: false,
-  data:[
-    { id:1, title:"Item 1"},
-    { id:2, title:"Item 2"},
-    { id:3, title:"Item 3"},
-        { id:11, title:"Item 1"},
-    { id:12, title:"Item 2"},
-    { id:13, title:"Item 3"},
-    { id:112, title:"Item 2"},
-    { id:113, title:"Item 3"}
-  ]
-});
+// if (!webix.env.touch && webix.ui.scrollSize) {
+//     webix.CustomScroll.init();
+// }
+//
+// var list = webix.ui({
+//   view:"list",
+//   width:250,
+//   height:200,
+//   template:"#title#",
+//   id:"list",
+//   select:true,
+//   // scroll: false,
+//   data:[
+//     { id:1, title:"Item 1"},
+//     { id:2, title:"Item 2"},
+//     { id:3, title:"Item 3"},
+//         { id:11, title:"Item 1"},
+//     { id:12, title:"Item 2"},
+//     { id:13, title:"Item 3"},
+//     { id:112, title:"Item 2"},
+//     { id:113, title:"Item 3"}
+//   ]
+// });
 
 
 // document.body.onmousewheel = function(event) {
@@ -63,3 +64,28 @@ var list = webix.ui({
 //     event.stopPropagation();
 //     event.preventDefault();
 // };
+
+// webix.ui({
+//     view:"window",
+//     id:"my_win",
+//     head: false,
+//     width: 100,
+//     body:{
+//         view:"list",
+//         template:"#title#",
+//         scroll: false,
+//         data:[
+//             { id:1, title:"权限管理"},
+//         ]
+//     }
+// }).show();
+
+// autowidth 属性
+var label = webix.ui({
+    view:"label",
+    label: "Labeddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddl",
+    align:"left",
+    autoheight: true,
+    width: 200,
+    line_height: 20
+});
